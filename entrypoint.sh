66e83a88ca2ed2b46d4b06497599496e4d77234d
Daniel Gerendasy <danielgerendasy@gmail.com>
2022-03-10 17:23:11 -0800

    linux update

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,17 @@ set -e
 # Allow the workdir to be set using an env var.
 # Useful for CI pipiles which use docker for their build steps
 # and don't allow that much flexibility to mount volumes
+SRCDIR=$1
+
+PYPI_URL=$2
+
+PYPI_INDEX_URL=$3
+
 WORKDIR=${SRCDIR:-/src}
+
+SPEC_FILE=${4:-*.spec}
+
+/root/.pyenv/shims/python -m pip install --upgrade pip wheel setuptools
 
 #
 # In case the user specified a custom URL for PYPI, then use
@@ -32,15 +42,9 @@ fi
 cd $WORKDIR
 
 if [ -f requirements.txt ]; then
-    pip install -r requirements.txt
+    /root/.pyenv/shims/pip install -r requirements.txt
 fi # [ -f requirements.txt ]
 
-echo "$@"
+/root/.pyenv/shims/pyinstaller --clean -y --dist ./dist/linux --workpath /tmp $SPEC_FILE
 
-if [[ "$@" == "" ]]; then
-    pyinstaller --clean -y --dist ./dist/linux --workpath /tmp *.spec
-    chown -R --reference=. ./dist/linux
-else
-    sh -c "$@"
-fi # [[ "$@" == "" ]]
-
+chown -R --reference=. ./dist/linux
